ae516ad62467c46996f8d31cc3a0dfad6d5b3f24
Aristides Staffieri <aristides.staffieri@stellar.org>
2023-07-05 14:52:41 -0600

    adds swapper A steps and state

--- src/components/atomic-swap/swapper-A.tsx
+++ src/components/atomic-swap/swapper-A.tsx
@@ -1,5 +1,12 @@
-import React from "react";
-import { Button, Heading, Select } from "@stellar/design-system";
+import React, { ChangeEvent } from "react";
+import {
+  Button,
+  Heading,
+  Select,
+  Input,
+  Profile,
+} from "@stellar/design-system";
+import BigNumber from "bignumber.js";
 import {
   WalletNetwork,
   WalletType,
@@ -8,13 +15,15 @@ import {
 } from "stellar-wallets-kit";
 
 import { bc, ChannelMessageType } from "helpers/channel";
+import { getServer, getTxBuilder, BASE_FEE, buildSwap } from "helpers/soroban";
+import { NetworkDetails, signTx } from "helpers/network";
 import { ERRORS } from "../../helpers/error";
 
-export type SwapperAStepCount = 1 | 2 | 3 | 4 | 5;
+export type SwapperAStepCount = 1 | 2 | 3 | 4;
 
 interface SwapperAProps {
   pubKey: string | null;
-  selectedNetwork: string;
+  networkDetails: NetworkDetails;
   setError: (error: string | null) => void;
   setPubKey: (pubKey: string) => void;
   setStepCount: (step: SwapperAStepCount) => void;
@@ -23,8 +32,14 @@ interface SwapperAProps {
 }
 
 export const SwapperA = (props: SwapperAProps) => {
-  const text = props.pubKey ? "Next" : "Connect Wallet";
   const [contractID, setContractID] = React.useState("");
+  const [tokenAAddress, setTokenAAddress] = React.useState("");
+  const [amountA, setAmountA] = React.useState("");
+  const [minAmountA, setMinAmountA] = React.useState("");
+  const [tokenBAddress, setTokenBAddress] = React.useState("");
+  const [amountB, setAmountB] = React.useState("");
+  const [minAmountB, setMinAmountB] = React.useState("");
+  const [swapperBAddress, setSwapperBAddress] = React.useState("");
   const connect = async () => {
     props.setError(null);
 
@@ -67,27 +82,271 @@ export const SwapperA = (props: SwapperAProps) => {
     }
   };
 
-  console.log(contractID);
+  function renderStep(step: SwapperAStepCount) {
+    switch (step) {
+      case 4: {
+        const signWithWallet = async () => {
+          const server = getServer(props.networkDetails);
+          // Gets a transaction builder and use it to add a "mint" operation and build the corresponding XDR
+          const txBuilder = await getTxBuilder(
+            props.pubKey!,
+            BASE_FEE,
+            server,
+            props.networkDetails.networkPassphrase,
+          );
 
-  return (
-    <>
-      <Heading as="h1" size="sm">
-        Choose Address A
-      </Heading>
-      <Select
-        disabled
-        fieldSize="md"
-        id="selected-network"
-        label="Select your Network"
-        value={props.selectedNetwork}
-      >
-        <option>{props.selectedNetwork}</option>
-      </Select>
-      <div className="submit-row">
-        <Button size="md" variant="tertiary" isFullWidth onClick={connect}>
-          {text}
-        </Button>
-      </div>
-    </>
-  );
+          const tokenA = {
+            id: tokenAAddress,
+            amount: new BigNumber(amountA).toNumber(),
+            minAmount: new BigNumber(minAmountA).toNumber(),
+          };
+
+          const tokenB = {
+            id: tokenBAddress,
+            amount: new BigNumber(amountB).toNumber(),
+            minAmount: new BigNumber(minAmountB).toNumber(),
+          };
+
+          const xdr = await buildSwap(
+            contractID,
+            tokenA,
+            tokenB,
+            props.pubKey!,
+            swapperBAddress,
+            "", // memo will be set after rebuild on exchange submit
+            server,
+            props.networkDetails.network,
+            txBuilder,
+          );
+
+          try {
+            // Signs XDR representing the "swap" transaction
+            const signedTx = await signTx(xdr, props.pubKey!, props.swkKit);
+            const newWindow = window.open(
+              `${window.location.href}swapper-b`,
+              "_blank",
+            );
+            if (newWindow) {
+              newWindow.onload = () => {
+                bc.postMessage({
+                  type: ChannelMessageType.SignedTx,
+                  data: signedTx,
+                });
+              };
+            }
+          } catch (e) {
+            console.log("e: ", e);
+            props.setError(ERRORS.UNABLE_TO_SIGN_TX);
+          }
+        };
+        return (
+          <>
+            <Heading as="h1" size="sm">
+              Confirm Swap Transaction
+            </Heading>
+            <div className="tx-details">
+              <div className="tx-detail-item">
+                <p className="detail-header">Network</p>
+                <p className="detail-value">{props.networkDetails.network}</p>
+              </div>
+              <div className="tx-detail-item">
+                <p className="detail-header">Address A</p>
+                <div className="address-a-identicon">
+                  <Profile isShort publicAddress={tokenAAddress} size="sm" />
+                </div>
+              </div>
+              <div className="tx-detail-item">
+                <p className="detail-header">Amount A</p>
+                <p className="detail-value">{amountA}</p>
+              </div>
+              <div className="tx-detail-item">
+                <p className="detail-header">Min Amount A</p>
+                <p className="detail-value">{minAmountA}</p>
+              </div>
+              <div className="tx-detail-item">
+                <p className="detail-header">Address B</p>
+                <div className="address-b-identicon">
+                  <Profile isShort publicAddress={tokenBAddress} size="sm" />
+                </div>
+              </div>
+              <div className="tx-detail-item">
+                <p className="detail-header">Amount B</p>
+                <p className="detail-value">{amountB}</p>
+              </div>
+              <div className="tx-detail-item">
+                <p className="detail-header">Min Amount B</p>
+                <p className="detail-value">{minAmountB}</p>
+              </div>
+            </div>
+            <div className="submit-row">
+              <Button
+                size="md"
+                variant="tertiary"
+                isFullWidth
+                onClick={signWithWallet}
+              >
+                Sign with Wallet
+              </Button>
+            </div>
+          </>
+        );
+      }
+      case 3: {
+        const handleSwapperBAddress = (
+          event: ChangeEvent<HTMLInputElement>,
+        ) => {
+          setSwapperBAddress(event.target.value);
+        };
+        const handleTokenBChange = (event: ChangeEvent<HTMLInputElement>) => {
+          setTokenBAddress(event.target.value);
+        };
+        const handleTokenBAmountChange = (
+          event: ChangeEvent<HTMLInputElement>,
+        ) => {
+          setAmountB(event.target.value);
+        };
+        const handleTokenBMinAmountChange = (
+          event: ChangeEvent<HTMLInputElement>,
+        ) => {
+          setMinAmountB(event.target.value);
+        };
+
+        return (
+          <>
+            <Heading as="h1" size="sm">
+              Choose Token B
+            </Heading>
+            <Input
+              fieldSize="md"
+              id="swapper-b-address"
+              label="Swapper B Address"
+              value={swapperBAddress}
+              onChange={handleSwapperBAddress}
+            />
+            <Input
+              fieldSize="md"
+              id="token-b-id"
+              label="Token ID"
+              value={tokenAAddress}
+              onChange={handleTokenBChange}
+            />
+            <Input
+              fieldSize="md"
+              id="token-b-amount"
+              label="Amount"
+              value={amountA}
+              onChange={handleTokenBAmountChange}
+            />
+            <Input
+              fieldSize="md"
+              id="token-b-min-amount"
+              label="Min Amount"
+              value={minAmountA}
+              onChange={handleTokenBMinAmountChange}
+            />
+            <div className="submit-row">
+              <Button
+                size="md"
+                variant="tertiary"
+                isFullWidth
+                onClick={() =>
+                  props.setStepCount((props.stepCount + 1) as SwapperAStepCount)
+                }
+              >
+                Next
+              </Button>
+            </div>
+          </>
+        );
+      }
+      case 2: {
+        const handleTokenAChange = (event: ChangeEvent<HTMLInputElement>) => {
+          setTokenAAddress(event.target.value);
+        };
+        const handleTokenAAmountChange = (
+          event: ChangeEvent<HTMLInputElement>,
+        ) => {
+          setAmountA(event.target.value);
+        };
+        const handleTokenAMinAmountChange = (
+          event: ChangeEvent<HTMLInputElement>,
+        ) => {
+          setMinAmountA(event.target.value);
+        };
+
+        return (
+          <>
+            <Heading as="h1" size="sm">
+              Choose Token A
+            </Heading>
+            <Input
+              fieldSize="md"
+              id="token-a-id"
+              label="Token ID"
+              value={tokenAAddress}
+              onChange={handleTokenAChange}
+            />
+            <Input
+              fieldSize="md"
+              id="token-a-amount"
+              label="Amount"
+              value={amountA}
+              onChange={handleTokenAAmountChange}
+            />
+            <Input
+              fieldSize="md"
+              id="token-a-min-amount"
+              label="Min Amount"
+              value={minAmountA}
+              onChange={handleTokenAMinAmountChange}
+            />
+            <div className="submit-row">
+              <Button
+                size="md"
+                variant="tertiary"
+                isFullWidth
+                onClick={() =>
+                  props.setStepCount((props.stepCount + 1) as SwapperAStepCount)
+                }
+              >
+                Next
+              </Button>
+            </div>
+          </>
+        );
+      }
+      case 1:
+      default: {
+        const text = props.pubKey ? "Next" : "Connect Wallet";
+        return (
+          <>
+            <Heading as="h1" size="sm">
+              Choose Address A
+            </Heading>
+            <Select
+              disabled
+              fieldSize="md"
+              id="selected-network"
+              label="Select your Network"
+              value={props.networkDetails.network}
+            >
+              <option>{props.networkDetails.network}</option>
+            </Select>
+            <div className="submit-row">
+              <Button
+                size="md"
+                variant="tertiary"
+                isFullWidth
+                onClick={connect}
+              >
+                {text}
+              </Button>
+            </div>
+          </>
+        );
+      }
+    }
+  }
+
+  return renderStep(props.stepCount);
 };
